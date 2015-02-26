3a168a174903ba4da12a37a15c3e7af7ef9b9854
akbhinge <abhinge@gmail.com>
2015-02-25 18:45:37 -0800

    Initial Commit

---
+++ DataTrackerClient/src/com/example/webservices/Login.java
@@ -0,0 +1,25 @@
+package com.example.webservices;
+
+
+
+//import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends Activity {
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		
+		Button login  = (Button) findViewById(R.id.button1);
+
+		
+	}
+
+	
+}
