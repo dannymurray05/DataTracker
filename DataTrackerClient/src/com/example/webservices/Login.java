ebbcf0f809e23d0f5a74dc7c51362565ce1208b6
akbhinge <abhinge@gmail.com>
2015-02-25 20:02:55 -0800

    Again made some changes to the display.

--- DataTrackerClient/src/com/example/webservices/Login.java
+++ DataTrackerClient/src/com/example/webservices/Login.java
@@ -3,23 +3,36 @@ package com.example.webservices;
 
 
 //import android.support.v7.app.ActionBarActivity;
+
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Login extends Activity {
 	
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		
 		Button login  = (Button) findViewById(R.id.button1);
-
 		
-	}
-
+		login.setOnClickListener(new View.OnClickListener()
+		{
+			
+			public void onClick(View v)
+			{
+				Toast.makeText(Login.this, "Invalid Username and Password",Toast.LENGTH_LONG).show();
+			}
+		}
+		);
+		 //android:layout_alignLeft="@+id/phn_text"
 	
+
+	}
 }
