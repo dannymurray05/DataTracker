29c95f9e094a9ce78e498a3a5d052b475e9c52d1
akbhinge <abhinge@gmail.com>
2015-05-02 11:37:53 -0700

    Committing the changes for settings tab! DataTrackerRequest.java file was giving errors after the pull request. Done with number picker dialog. Now you can set the billing cycle and accordingly pass to database. Did not make any other changes to avoid merge conflicts.
    After login, the app somehow does not go to settings/data report part
    directly. After multiple attempts, it goes to the page.
    
    Did not edit anything else to avoid any conflicts.

--- DataTrackerClient/src/datatrackerclient/DataTracker.java
+++ DataTrackerClient/src/datatrackerclient/DataTracker.java
@@ -1,19 +1,27 @@
 package datatrackerclient;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.datatrackerclient.R;
 
 import datatrackerclient.sessionmanagement.Login;
 import datatrackerclient.sessionmanagement.SessionManager;
 
-public class DataTracker extends Activity {
+public class DataTracker extends Activity implements NumberPicker.OnValueChangeListener{
 
-	
+	private TextView tv;
+    static Dialog d ;
 	public static final int LOGIN_REQUEST = 0b1;
 	//DatePicker getdate;
 	
@@ -54,6 +62,9 @@ public class DataTracker extends Activity {
 		setContentView(R.layout.activity_datatracker);
 		//getdate = (DatePicker) findViewById(R.id.billing_date);
 		
+		
+		
+		
 		final TabHost tabHost = (TabHost) findViewById(R.id.tabhost);
 		tabHost.setup();
 		
@@ -67,14 +78,65 @@ public class DataTracker extends Activity {
 		
 		tabHost.addTab(spec1);
 		tabHost.addTab(spec2);
-	}
+		
+		
+		tv = (TextView) findViewById(R.id.set_cycle);
+        Button b = (Button) findViewById(R.id.get_cycle);// on click of button display the dialog
+         b.setOnClickListener(new View.OnClickListener()
+         {
 
-/*	public void SetCycle(View v)
-	{
-		int day = getdate.getDayOfMonth();
-		int month = getdate.getMonth() + 1;
-		int year = getdate.getYear();
-		Toast.makeText(getBaseContext(), "Date:"+day+"/"+month+"/"+year, Toast.LENGTH_LONG).show();
-	} */
+            @Override
+            public void onClick(View v) {
+                 show();
+            }
+            });
+           }
+		
+		
+		
+	//	NumberPicker np = (NumberPicker)findViewById(R.id.set_cycle);
+	  //  np.setMinValue(1);
+	    //np.setMaxValue(31);
+	    //np.setWrapSelectorWheel(true); 
+	
+	    //Toast.makeText(getApplicationContext(),  "Billing Cycle is: " + np.getValue() +"",Toast.LENGTH_SHORT).show();
+	    
+	
+
+	@Override
+	public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+		// TODO Auto-generated method stub
+		 Log.i("value is",""+newVal);
+		
+		}
+	
+	public void show()
+    {
+
+         final Dialog d = new Dialog(DataTracker.this);
+         d.setTitle("Billing Cycle");
+         d.setContentView(R.layout.activity_dialog);
+         Button b1 = (Button) d.findViewById(R.id.set_button);
+         final NumberPicker np = (NumberPicker) d.findViewById(R.id.pick_cycle);
+         np.setMaxValue(31); 
+         np.setMinValue(1);   
+         np.setWrapSelectorWheel(true);
+         np.setOnValueChangedListener(this);
+        
+         b1.setOnClickListener(new View.OnClickListener()
+         {
+          @Override
+          public void onClick(View v) {
+              tv.setText(String.valueOf(np.getValue())); //set the value to textview
+              d.dismiss();
+           }    
+          });
+        
+       d.show();
+
+
+    }
+	
+	
 	
 }
